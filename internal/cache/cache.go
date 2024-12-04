a50a20e995befbc48bab75ea8620f91ec0c31efb
Matt Gleich <git@mattglei.ch>
2024-12-04 16:13:27 -0500

    feat: properly check for warning error
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -85,7 +85,7 @@ func (c *Cache[T]) Update(data T) {
 		lumber.Error(err, "failed to json marshal new data")
 		return
 	}
-	if string(old) != string(new) && string(new) != "null" {
+	if string(old) != string(new) && string(new) != "null" && strings.Trim(string(new), " ") != "" {
 		c.data = data
 		c.updated = time.Now()
 		updated = true
@@ -104,8 +104,12 @@ func (c *Cache[T]) StartPeriodicUpdate(updateFunc func() (T, error), interval ti
 	defer ticker.Stop()
 	for range ticker.C {
 		data, err := updateFunc()
-		if err != nil && !errors.Is(err, apis.WarningError) {
-			lumber.Error(err, "updating", c.name, "cache failed")
+		if err != nil {
+			if errors.Is(err, apis.WarningError) {
+				lumber.Warning(err, "updating", c.name, "cache failed")
+			} else {
+				lumber.Error(err, "updating", c.name, "cache failed")
+			}
 			continue
 		}
 		c.Update(data)
